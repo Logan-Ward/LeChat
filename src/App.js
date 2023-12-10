778bea84d7a67320443bba86a339eda3c3f6d777
Logan-Ward <ward_logan@outlook.com>
2023-12-09 23:39:33 -0600

    Changing workspace

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='left-bar'></div>
+      <div className='right-bar'></div>
+      <div className='center-bar'></div>
+      <div className='top-bar'></div>
+      <div className='bottom-bar'></div>
+    </div>
+  );
+}
+
+export default App;
